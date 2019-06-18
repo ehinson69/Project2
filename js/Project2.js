4ec3ddc1ecc4775d59410ab71238c22f1f1cfa7d
Liz Hinson <elizabeth.hinson@verizon.com>
2019-06-18 14:44:13 -0500

    Setting up Project2

---
+++ js/Project2.js
@@ -0,0 +1,22 @@
+/*****FSJS Project 2 - List Filter and Pagination
+ * By Elizabeth Hinson
+ * June 21, 2019
+ */
+
+ /*****Global Variables Storing the DOM Elements */
+
+
+
+
+
+
+
+
+ /*****showPage Function */
+
+
+
+
+
+
+ /*****appendPageLinks Function */
