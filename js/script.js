13a01b3a1ebd1e3e0ebf83f1de6fbcfbe137293c
Liz Hinson <elizabeth.hinson@verizon.com>
2019-06-20 13:31:15 -0500

    started the project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 /******************************************
 Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
+FSJS Project 2 - List Filter and Pagination
 By Elizabeth Hinson
 ******************************************/
-   
+
+
+
+
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
@@ -17,9 +21,10 @@ By Elizabeth Hinson
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
+let numberOfItems = 10;
+const studentInfo = document.querySelectorAll('.student-item');
 
-
-
+console.log(studentInfo);
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -35,17 +40,26 @@ By Elizabeth Hinson
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+const showPage = (list, page) => {
+
+for (let i = 0; i < numberOfItems.length, i +=1; {
+   numberofItems[10].studentInfo;
+
+}
 
 
 
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+function 
+/***targets an element on a page that set up the callback to fire in resonse to the specified event. */
+eventTarget.addEventListener(
 
-
-
-
+)
+target.addEventListener(type, listener[,options]);
+window.setTimeout(add.5000,2,2);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
