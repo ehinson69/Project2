36975500bb7a3c5304c39b88e3ca12b7931a56e9
Liz Hinson <elizabeth.hinson@verizon.com>
2019-06-20 14:49:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,66 +4,42 @@ FSJS Project 2 - List Filter and Pagination
 By Elizabeth Hinson
 ******************************************/
 
-
-
-
-
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+   Created two global variables to store in the DOM, which will call a list of 10 students and student information on a page.***/
 let numberOfItems = 10;
 const studentInfo = document.querySelectorAll('.student-item');
 
 console.log(studentInfo);
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-const showPage = (studentInfo, numberOfItems) => {
-   var i;
-   var page = numberOfItems;
-      for (let i = 0; i < numberOfItems.length, i++) {
-         if(numberOfItems[10].studentInfo) {
-         showPage = numberOfItems[10];
+   Created a showPage function to hide all of the items on a page except 10 at a time. ***/
+const showPage = (studentInfo, page) => {
+   const startIndex = (page * numberOfItems) - numberOfItems;
+   const endIndex = page * numberOfItems;
+      for let i = 0; i < studentInfo.length; i++) {
+      i >= startIndex && i < endIndex;   
       }
-   }
-   passListData(showPage);
-}
-
+   } 
+   showPage();
+   
+// if(numberOfItems[10].studentInfo) {
+ //  showPage = numberOfItems[10];
+   //
 
 /***
  Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-const appendPageLinks = () => {
+// const appendPageLinks = () => {
 
-}
+// }
 /***targets an element on a page that set up the callback to fire in resonse to the specified event. */
-eventTarget.addEventListener(
+// eventTarget.addEventListener(
 
-)
-target.addEventListener(type, listener[,options]);
-window.setTimeout(add.5000,2,2);
+// )
+// target.addEventListener(type, listener[,options]);
+// window.setTimeout(add.5000,2,2);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
