2df9cce49f098ce4b3d304e3988a87a964d9a272
Liz Hinson <elizabeth.hinson@verizon.com>
2019-06-21 14:38:43 -0500

    figuring out the append section

--- js/script.js
+++ js/script.js
@@ -6,16 +6,14 @@ By Elizabeth Hinson
 
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-/*** 
-   Created two global variables to store in the DOM, which will call a list of 10 students and student information on a page.***/
+/***Created two global variables to store in the DOM, which will call a list of 10 students and student information on a page.***/
 let numberOfItems = 10;
 const studentInfo = document.querySelectorAll('.student-item');
 
 console.log(numberOfItems);
 console.log(studentInfo);
 
-/*** 
-   Created a showPage function to hide all of the items on a page except 10 at a time. For page one just show items 0-9, 
+/***Created a showPage function to hide all of the items on a page except 10 at a time. For page one just show items 0-9, 
    for page two just show 10-19, and so forth, while hiding the rest ***/
 const showPage = (studentInfo, page) => {
    let startIndex = (page * numberOfItems) - numberOfItems;
@@ -28,20 +26,34 @@ const showPage = (studentInfo, page) => {
          }
       }
 }; 
-   showPage(studentInfo, 1);
+   showPage(studentInfo, 1); //show result with new search
    
 /***
  Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-// const appendPageLinks = () => {
+const appendPageLinks = (studentInfo) => {
+   let div = document.createElement("div");
+   let ul = document.createElement("ul");
+   let li = document.createElement("page");
+   div = setAttribute('pagination');
+   ul = 'li';
 
-// }
+   document.addEventListener('appendPageLinks');
+   appendPageLinks(0); 
+ }
+ if($('.pagination').length === 0){
+   $('.page').append(`<div class="${pagination}">
+       <ul></ul>
+   </div>
+`);
+} 
+
 /***targets an element on a page that set up the callback to fire in resonse to the specified event. */
-// eventTarget.addEventListener(
+ eventTarget.addEventListener(
 
-// )
-// target.addEventListener(type, listener[(numberOfItems),options]);
-// window.setTimeout(add.5000,2,2);
+ )
+ target.addEventListener(type, listener[(numberOfItems),options]);
+ window.setTimeout(add,5000,2,2);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
