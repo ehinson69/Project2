6a4cdfbe27f0241afd3c72467412cc7c1951e8fd
Liz Hinson <elizabeth.hinson@verizon.com>
2019-06-22 19:28:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,44 +7,44 @@ By Elizabeth Hinson
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 /***Created two global variables to store in the DOM, which will call a list of 10 students and student information on a page.***/
-const numberOfItems = 10;
 const studentInfo = document.querySelectorAll('.student-item');
+const numberOfItems = 10;
+//console.log(numberOfItems);
+//console.log(studentInfo);
 
-console.log(numberOfItems);
-console.log(studentInfo);
-
-function getNumberOfPages(){
-   return Math.ceil(studentInfo.length/itemsPerPage);
+function getNumberOfPages() {
+   return Math.ceil(studentInfo.length / numberOfItems);
 }
 
 /***Created a showPage function to hide all of the items on a page except 10 at a time. For page one just show items 0-9, 
    for page two just show 10-19, and so forth, while hiding the rest ***/
-const showPage = (studentInfo, page) => {
+function showPage(studentInfo, page) {
    let startIndex = (page * numberOfItems) - numberOfItems;
    let endIndex = page * numberOfItems;
-      for (let i = 0; i < studentInfo.length; i++) {
-         if (i >= startIndex && i < endIndex) {
-            studentInfo[i].style.display = 'block'; //showing
-         } else {
-            studentInfo[i].style.display = 'none'; //hiding
-         }
+
+   for (let i = 0; i < studentInfo.length; i ++) {
+      if (i >= startIndex && i < endIndex) {
+         studentInfo[i].style.display = 'block'; //showing
+      } else {
+         studentInfo[i].style.display = 'none'; //hiding
       }
-}; 
-   showPage(studentInfo, 1); //show result with new search
-   
+   }
+}
+//showPage(studentInfo, textContent);
+     
 /***Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.***/
 
-const appendPageLinks = (studentInfo) => {
+function appendPageLinks() {
    let page = document.createElement("page");
    let div = document.createElement("div");
    let ul = document.createElement("ul");
-   div.className=('pagintion');
+   div.className=('pagination');
    div.setAttribute('class', 'pagination');
    page.appendChild(div);
    div.appendChild(ul);
 
-   for(let i = 1; 1 <= getNumberOfPages(studentInfo); i++){
+   for(let i = 1; 1 <= getNumberOfPages(studentInfo); i++) {
       let li = document.createElement('li');
       let a = document.createElement('a');
       a.setAttribute('href', '#');
@@ -61,6 +61,9 @@ const appendPageLinks = (studentInfo) => {
       });
    }
 }  
-//showpage(studentList, textContent);
-showPage(studentList, 1);
-appendPageLinks;
+
+//showPage(studentList, textContent);
+showPage(studentInfo,1);
+appendPageLinks();
+
+//let link = document.querySelectAll('.pagination a')
